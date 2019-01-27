b1d7a783b8321a727040ac81e7f7156ec588a750
Jorge Ribeiro <joorgemelo@gmail.com>
2019-01-26 23:04:42 -0300

    Long distance filter implemented

--- src/main/java/com/glovoapp/backender/OrderService.java
+++ src/main/java/com/glovoapp/backender/OrderService.java
@@ -14,15 +14,25 @@ class OrderService {
 	@Value("#{'${backender.order.box}'.split(',')}")
 	List<String> boxOrders;
 	
+	@Value("${backender.order.long_distance_limit_km}")
+	Double longDistanceLimit;
+	
 	List<Order> getOrdersByCourier(Courier courier, List<Order> orders) {
 		return orders
 				.stream()
 				.filter(order -> filterBoxOrders(courier.getBox(), order.getDescription()))
+				.filter(order -> filterLongDistanceOrders(courier.getVehicle(), courier.getLocation(), order.getPickup()))
 				.collect(Collectors.toList());
 	}
 	
-	private Boolean filterBoxOrders(Boolean hasBox, String description) {
-		return hasBox || !boxOrders.stream().anyMatch(boxWord -> StringUtils.containsIgnoreCase(description, boxWord));
+	private Boolean filterBoxOrders(Boolean hasBox, String orderDescription) {
+		return hasBox || !boxOrders.stream().anyMatch(boxWord -> StringUtils.containsIgnoreCase(orderDescription, boxWord));
+	}
+	
+	private Boolean filterLongDistanceOrders(Vehicle vehicleType, Location courierLocation, Location orderPickup) {
+		return vehicleType == Vehicle.ELECTRIC_SCOOTER || 
+				vehicleType == Vehicle.MOTORCYCLE ||
+				DistanceCalculator.calculateDistance(courierLocation, orderPickup) <= longDistanceLimit;
 	}
 	
 }
